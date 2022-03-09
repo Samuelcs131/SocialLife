f0492e447d44eaad3dd5483a18496b8771c373fc
Samuel Claudino <samuelcs131@gmail.com>
2022-03-09 18:57:45 -0300

    upload project

---
+++ src/components/Menu.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from '../styles/styles'
+
+const Menu = () => {
+    return ( 
+        <Navbar>
+            <img src="/img/logo-socialLife.svg" alt="icone socialLife" />
+            <span>SocialLife</span>
+        </Navbar>
+     );
+}
+ 
+export default Menu;
---
+++ src/styles/colorPalette.ts
@@ -0,0 +1,16 @@
+/* COLORS */ 
+let colorPalette = {
+    white: '#FFFFFF',
+    black100: '#040C11',
+    black200: '#1b2730',
+    black300: '#05141c',
+    black400: '#3B3B3B',
+    black500: '#28353e',
+    grey100: '#4B4B4B',
+    grey200: '#45525d',
+    grey300: '#b9b9b9',
+    grey400: '#d9d9d9',
+    blue: '#1ca1f1',
+};
+
+export default colorPalette;
